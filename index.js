b76a31adac1db0df5712a7ed11191dbba98f624c
shakeebshams <shakeebshams@gmail.com>
2021-05-25 16:34:53 -0400

    fix relay port issues 3

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function send_text(message, to_number){
         .then(message => console.log(message.sid));
 }
 
-const app = relay({server: server});
+const app = relay({'server': server});
 app.workflow(`twilio`, relay => {
     console.log("app is hosted and running")
     let message = ''
