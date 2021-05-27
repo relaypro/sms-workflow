ed8a578e3c4d5ae295db9eca5360077373fe35c7
shakeebshams <shakeebshams@gmail.com>
2021-05-27 17:32:13 -0400

    revert await changes

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ server.post('/', function(req, res) {
 
     let number = parseNumber(req.body.num1, 'USA')
     if (number.length === 0) {
-        await alert.fire("Number not recognized, please try again")
-        res.redirect('/')
+        alert.fire("Number not recognized, please try again")
+        //res.redirect('/')
     } else {
         console.log(number)
         const post = new SmsDB({
