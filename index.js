bbcb9aeab5b197363bdfdc08ecfd36ad34ad650c
shakeebshams <shakeebshams@gmail.com>
2021-06-01 09:50:11 -0400

    major UI changes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import { relay } from '@relaypro/sdk'
 import twilio from './workflows/twilio.js'
 import SmsDB from './schemas/SmsDB.js'
 import parseNumber from 'phone'
-import alert from 'sweetalert2'
 import EventEmitter from 'events'
+import alert2 from 'alert'
 
 export const eventEmitter = new EventEmitter()    
 const port = process.env.PORT || 3000
@@ -34,19 +34,29 @@ _server.get('/generate', function(req, res) {
 _server.post('/generate', function(req, res) {
     let device_id = req.body.user_id
     let url = "relay-sms.herokuapp.com/id/" + device_id.toString()
-    res.send("<div><p>" + url + "<p></div>")
+    alert2(url)
+    res.redirect("/generate")
 })
 _server.get('/main.js', function(req, res) {
     res.sendFile(path.join(__dirname, '/main.js'))
 })
 
+_server.get('/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname, '/style.css'))
+})
+
+_server.get('/logo.png', function(req, res) {
+    res.sendFile(path.join(__dirname, '/logo.png'))
+})
+
 _server.post('/', function(req, res) {
     console.log("user number recieved: " + req.body.num1)
     console.log("name recieved: " + req.body.name)
-    id = req.body.user_id ? req.body.user_id : id 
+    id = req.body.user_id ? req.body.user_id : id
+    console.log(id)
     let number = parseNumber(req.body.num1, 'USA')
     if (number.length === 0) {
-        alert.fire("Number not recognized, please try again")
+        alert2("Number not recognized, please try again")
         res.redirect('/')
     } else {
         console.log(number)
@@ -57,7 +67,7 @@ _server.post('/', function(req, res) {
         })
         post.save(function(err){
             if (!err){
-                res.send("recieved")
+                res.sendFile(path.join(__dirname, '/recieved.html'))
             } else {
                 console.log(err)
             }
