e4dc298461e9f20011b871a12b2f9de1772f43ee
shakeebshams <shakeebshams@gmail.com>
2021-06-02 13:32:40 -0400

    restructuring and hiding env vars

--- schemas/SmsDB.js
+++ schemas/SmsDB.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
-//const uri = process.env.MONGODB_URI
-const uri = "mongodb+srv://relay-1:relayrelay@relaysms.svodb.mongodb.net/relaysms?retryWrites=true&w=majority"
+const uri = process.env.MONGODB_URI
 mongoose.connect(uri, {useNewUrlParser: true})
 const relayTwilioSchema = {
     name: String,
